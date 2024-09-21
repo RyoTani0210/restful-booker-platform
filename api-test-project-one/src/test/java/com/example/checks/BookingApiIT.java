fa858b24f853cc60048cb1b4d4a0d13ebc1f20ce
RyoTani0210 <ryo.taniguchi@gmail.com>
2024-09-21 15:04:25 +0900

    テストを作成

--- api-test-project-one/src/test/java/com/example/checks/BookingApiIT.java
+++ api-test-project-one/src/test/java/com/example/checks/BookingApiIT.java
@@ -1,9 +1,14 @@
 package com.example.checks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
+import com.example.payloads.Booking;
+import com.example.payloads.BookingDates;
 import com.example.requests.BookingApi;
 
 import io.restassured.response.Response;
@@ -11,9 +16,27 @@ import io.restassured.response.Response;
 public class BookingApiIT{
     @Test
     public void getBookingSummaryShouldReturn200(){
+        // GETリクエスト
         Response response = BookingApi.getBookingSummary();
 
         assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    public void postBookingReturns201(){
+        BookingDates dates = new BookingDates(
+            LocalDate.of(2021, 1, 1),
+            LocalDate.of(2021,1,3)
+        );
+
+        Booking payload = new Booking(
+            1,
+            "John",
+            "Smith",
+            true,
+            dates,
+            "Breakfast"
+        );
     }
 
 }
